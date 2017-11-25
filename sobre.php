62682a64273fe9fbf61615a39f8e5ffb6cce0275
Samuel <samuel.t.almeida@gmail.com>
2017-11-25 21:08:15 -0200

    correcao css sobre.php

--- sobre.php
+++ sobre.php
@@ -1,5 +1,5 @@
 <?php include("header.php");?>
-<div id="description">
+<div id="header">
 	<span id="siteName"><i class="fa fa-eye" aria-hidden="true"></i> Spotted <i class="fa fa-eye" id="eye" 
 	aria-hidden="true"> </i></span> 
 	<br>
@@ -8,11 +8,7 @@
 	<br>
 	<br>		
 	<div id="description">
-			Spotted é uma ferramenta gratuita onde se pode 
-			deixar mensagens para que outros possam
-			ler, parecido com anúncio de jornais. Você pode simplesmente
-			desabafar, mandar uma cantada para aquele(a) garoto(a) de quem
-			você gostou, jogar papo fora e até mesmo usar como achados e perdidos.
+			Spotted é uma ferramenta gratuita onde se pode deixar mensagens para que outros possam ler, parecido com anúncio de jornais. Você pode simplesmente desabafar, mandar uma cantada para aquele(a) garoto(a) de quem você gostou, jogar papo fora e até mesmo usar como achados e perdidos.
 	</div>
 </div> <!-- description div end -->
 <?php include("footer.php");?>
