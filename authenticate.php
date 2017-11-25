059b8417f7e6a1403e9dfbfe38b7a36fc2078e98
Samuel <samuel.t.almeida@gmail.com>
2017-11-25 16:32:54 -0200

    Insert post e Auth da Sessao

---
+++ authenticate.php
@@ -0,0 +1,14 @@
+<?php
+  session_start();
+
+  if (isset($_SESSION["idusuario"]) && isset($_SESSION["nickname"]) && isset($_SESSION["senha"])) {
+    $login = true;
+    $idusuario = $_SESSION["idusuario"];
+    $nickname = $_SESSION["nickname"];
+    $senha = $_SESSION["senha"];
+  }
+  else{
+    $login = false;
+  }
+
+?>
