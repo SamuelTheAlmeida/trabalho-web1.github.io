4d6262c06f3efc85b1a9d626ba239f06b479dd9e
Samuel <samuel.t.almeida@gmail.com>
2017-11-26 18:56:16 -0200

    alterar senha funcionando

--- account.php
+++ account.php
@@ -9,98 +9,50 @@ $success = false;
 $loginerror = false;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if ($_POST['submit'] == 'alterar') {
-     if (!empty($_POST["nicknameLogin"]) && !empty($_POST["senhaLogin"]) && !empty($_POST["confirmarSenhaLogin"])) {
+     if (!empty($_POST["nicknameLogin"]) && !empty($_POST["senhaLogin"]) && !empty($_POST["novaSenhaLogin"])) {
 
         $conn = connect_db();
 
         $nicknameLogin = mysqli_real_escape_string($conn,$_POST["nicknameLogin"]);
         $senhaLogin = mysqli_real_escape_string($conn,$_POST["senhaLogin"]);
-        $senhaLogin = md5($senhaLogin);
+        $novaSenhaLogin = mysqli_real_escape_string($conn,$_POST["novaSenhaLogin"]);
 
-        $sql = "SELECT * FROM usuarios
-                WHERE nickname = '".$nicknameLogin."';";
-
-        $result = mysqli_query($conn, $sql);
-        if($result){
-          if (mysqli_num_rows($result) > 0) {
-            $usuario = mysqli_fetch_assoc($result);
-
-            if ($usuario["senha"] == $senhaLogin) {
-
-              $_SESSION["idusuario"] = $usuario["idusuario"];
-              $_SESSION["senha"] = $usuario["senha"];
-              $_SESSION["nickname"] = $usuario["nickname"];
-              $_SESSION["telefone"] = $usuario["telefone"];
-              $_SESSION["email"] = $usuario["email"];
-
-              // $success = true;
-              // $success_msg = "Login realizado com sucesso.";
-              header("Location: " . dirname($_SERVER['SCRIPT_NAME']) . "/login.php");
-              // exit();
-            }
-            else {
-              $error_msg = "Senha incorreta!";
-              $error = true;
-            }
-          }
-          else{
-            $error_msg = "Usuário não encontrado!";
-            $error = true;
-          }
-        }
-        else {
-          $error_msg = mysqli_error($conn);
+        if ($senhaLogin == $novaSenhaLogin) {
           $error = true;
+          $error_msg = "A nova senha não pode ser igual à senha atual.";
+        } else {
+            $senhaLogin = md5($senhaLogin);
+            $novaSenhaLogin = md5($novaSenhaLogin);
+
+            $sql = "SELECT usuarios.senha FROM usuarios
+            WHERE nickname = '".$nicknameLogin."';";
+
+            $result = mysqli_query($conn, $sql);
+
+            if($result) {
+              $row = mysqli_fetch_assoc($result);
+              if ($row["senha"] == $senhaLogin) {
+                $query = "UPDATE usuarios SET senha = '".$novaSenhaLogin."' WHERE nickname = '".$nicknameLogin."' ; ";
+                $result = mysqli_query($conn, $query);
+                if ($result) {
+                  $success = true;
+                  $success_msg = "Senha alterada com sucesso";
+                } else {
+                  $error = true;
+                  $error_msg = "Não foi possível alterar a senha.";
+                }
+              } else {
+                $error = true;
+                $error_msg = "Senha atual incorreta.";
+              }
+            } 
         }
       }
-      else {
-        $error_msg = "Por favor, preencha todos os dados no login.";
-        $error = true;
-      }
-
   }
-  else if ($_POST['submit'] == 'cadastrar') {
-    if (!empty($_POST['nicknameCadastro']) && !empty($_POST['senhaCadastro']) && !empty($_POST['confirmSenhaCadastro'])) {
-      $conn = connect_db();
+        
+        
+        
 
-      $nicknameCadastro = mysqli_real_escape_string($conn,$_POST["nicknameCadastro"]);
-      $senhaCadastro = mysqli_real_escape_string($conn,$_POST["senhaCadastro"]);
-      $confirmSenhaCadastro = mysqli_real_escape_string($conn,$_POST["confirmSenhaCadastro"]);
-      if (isset($_POST["telefoneCadastro"])) {
-        $telefone = mysqli_real_escape_string($conn,$_POST["telefoneCadastro"]);
-      }
-      if (isset($_POST["emailCadastro"])) {
-        $email = mysqli_real_escape_string($conn,$_POST["emailCadastro"]);
-      }
-
-      if ($senhaCadastro == $confirmSenhaCadastro) {
-        $senhaCadastro= md5($senhaCadastro);
-
-        $sql = "INSERT INTO usuarios
-                (nickname, senha, telefone, email) VALUES
-                ('".$nicknameCadastro."', '".$senhaCadastro."', '".$telefone."', '".$email."');";
-
-        if(mysqli_query($conn, $sql)){
-          $success = true;
-          $success_msg = "Usuario cadastrado com sucesso";
-        }
-        else {
-          $error_msg = mysqli_error($conn);
-          $error = true;
-        }
-      }
-      else {
-        $error_msg = "Senha não confere com a confirmação.";
-        $error = true;
-      }
-    }
-    else {
-      $error_msg = "Por favor, preencha todos os dados obrigatórios.";
-      $error = true;
-    }
-  }
-}
 ?>
 <?php if ($success): ?>
     <h3 style="color:lightgreen;"><?= $success_msg ?></h3>
@@ -124,9 +76,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <?php endif; ?>
 	  
 	  <form id="loginForm" action="account.php" method="POST" class="loginForm">
-      <input type="text" placeholder="Nickname" name="nicknameLogin" class="loginInputs"> <br>
-      <input type="password" placeholder="senha" name="senhaLogin" class="loginInputs">
-  		<input type="password" placeholder="senha" name="confirmarSenhaLogin" class="loginInputs">
+      <input  minlength="3" maxlength="20" type="text" placeholder="Nickname" name="nicknameLogin" class="loginInputs"> <br>
+      <input minlength="8" maxlength="32" required="required" type="password" type="password" placeholder="senha" name="senhaLogin" class="loginInputs">
+  		<input minlength="8" maxlength="32" required="required" type="password" type="password" placeholder="senha" name="novaSenhaLogin" class="loginInputs">
       <button id="alterar" class="submit" name="submit" value="alterar">Alterar</button>
 		<span><br></span><br>
     </form>
